d0e0a3f5a85a8badfd97a303424e56f3a8ae8db5
zperkowski <ZdzislawPerkowski@outlook.com>
2018-09-10 21:20:48 +0200

    Generating list of batches, PEP name convention fix

--- Eye.py
+++ Eye.py
@@ -2,7 +2,6 @@ from skimage import color
 import numpy as np
 from random import randint
 from matplotlib import pyplot as plt
-import cv2
 
 
 class Eye:
@@ -14,44 +13,55 @@ class Eye:
     __mask = None
     __calculated = None
     patchSize = 5
-    offset = int()
     x = int()
     y = int()
 
     def __init__(self, raw, manual, mask, patchSize):
-        self.__raw = cv2.resize(raw, dsize=(int(raw.shape[1]*0.5), int(raw.shape[0]*0.5)), interpolation=cv2.INTER_CUBIC)
-        self.__manual = cv2.resize(manual, dsize=(int(manual.shape[1]*0.5), int(manual.shape[0]*0.5)), interpolation=cv2.INTER_CUBIC)
-        self.__mask = cv2.resize(mask, dsize=(int(mask.shape[1]*0.5), int(mask.shape[0]*0.5)), interpolation=cv2.INTER_CUBIC)
+        self.__raw = raw
+        self.__manual = manual
+        self.__mask = mask
         self.__calculated = np.zeros(self.__manual.shape)
         self.__rawGrey = color.rgb2gray(self.__raw)
         self.patchSize = patchSize
-        self.offset = int(patchSize / 2)
-        self.x = self.y = 0 + self.offset
 
-    def getRaw(self):
+    def get_raw(self):
         return self.__raw
 
-    def getManual(self):
+    def get_manual(self):
         return self.__manual
 
-    def getMask(self):
+    def get_mask(self):
         return self.__mask
 
-    def getCalculated(self):
+    def get_calculated(self):
         return self.__calculated
 
+    def _get_batches(self, picture):
+        batches = []
+        for y in range(0, picture.shape[1] - self.patchSize):
+            for x in range(0, picture.shape[0] - self.patchSize):
+                sub_picture = picture[y:y+self.patchSize, x:x+self.patchSize]
+                batches.append(sub_picture)
+        return batches
+
+    def get_batches_of_raw(self):
+        return self._get_batches(self.get_raw())
+
+    def get_batches_of_manual(self):
+        return self._get_batches(self.get_manual())
+
     def compare(self):
-        w = self.getCalculated().shape[0]
-        h = self.getCalculated().shape[1]
+        w = self.get_calculated().shape[0]
+        h = self.get_calculated().shape[1]
         total_pixels = 0
         difference = 0.0
 
         for x in range(w):
             for y in range(h):
-                if (self.getMask()[x][y][0] == self.getMask()[x][y][1] == self.getMask()[x][y][2] == 255):
-                    if (self.getManual()[x][y] != self.getCalculated()[x][y]):
+                if self.get_mask()[x][y][0] == self.get_mask()[x][y][1] == self.get_mask()[x][y][2] == 255:
+                    if self.get_manual()[x][y] != self.get_calculated()[x][y]:
                         total_pixels += 1
-                        difference += abs((self.getManual()[x][y] / 255) - (self.getCalculated()[x][y] / 255))
+                        difference += abs((self.get_manual()[x][y] / 255) - (self.get_calculated()[x][y] / 255))
 
         if (total_pixels > 0):
             difference /= total_pixels
@@ -59,26 +69,26 @@ class Eye:
             difference = 0.0
         return difference
 
-    def buildImage(self, classification, threshold=0.5):
+    def build_image(self, classification, threshold=0.5):
         flat_calculated = np.zeros(classification.shape)
         t = np.max(classification) * threshold
         for y in range(int(classification.shape[1])):
             for x in range(int(classification.shape[0])):
-                if (classification[x, y] > t):
+                if classification[x, y] > t:
                     flat_calculated[x, y] = 255
 
         self.__calculated = flat_calculated
 
-    def plotRaw(self, extraStr=''):
-        self.plotImage(self.getRaw(), 'Raw ' + str(extraStr))
+    def plot_raw(self, extraStr=''):
+        self.plot_image(self.get_raw(), 'Raw ' + str(extraStr))
 
-    def plotManual(self, extraStr=''):
-        self.plotImage(self.getManual(), "Manual " + str(extraStr))
+    def plot_manual(self, extraStr=''):
+        self.plot_image(self.get_manual(), "Manual " + str(extraStr))
 
-    def plotCalculated(self, extraStr=''):
-        self.plotImage(self.getCalculated(), "Calculated " + str(extraStr))
+    def plot_calculated(self, extraStr=''):
+        self.plot_image(self.get_calculated(), "Calculated " + str(extraStr))
 
-    def plotImage(self, image, title=''):
+    def plot_image(self, image, title=''):
         plt.imshow(image, cmap='gray')
         plt.title(title)
         plt.show()
